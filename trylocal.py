52b598f2bb989208a26850bc5a41387d17fb5c85
JabinGP <1748511181@qq.com>
2021-09-18 16:00:45 +0800

    update

--- trylocal.py
+++ trylocal.py
@@ -8,12 +8,12 @@ from werkzeug.local import(
 l = Local()
 l.user = {"id": 1}
 
-print(f"l.user = %s"%l.user)
+print("l.user = %s" % l.user)
 
 user = l.user
-print(f"user = %s"%user)
+print("user = %s" % user)
 user_proxy = LocalProxy(l, "user")
-print(f"user_proxy = %s"%user_proxy)
+print("user_proxy = %s" % user_proxy)
 l.user = {"ident": 199}
-print(f"user_proxy = %s"%user_proxy)
-print(f"user = %s"%user)
+print("user_proxy = %s" % user_proxy)
+print("user = %s" % user)
